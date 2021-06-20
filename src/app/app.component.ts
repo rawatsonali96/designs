efeb3f723921722cd5d676bc545f4d893ce84fc7
sonali.rawat <sonali.rawat@kdi.kongsberg.com>
2021-06-21 00:32:34 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Options } from '@angular-slider/ngx-slider';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TemplateDesign';
+  incomeValue: number = 100000;
+  incomeOptions: Options = {
+    floor: 100000,
+    ceil: 300000
+  };
+
+  expenseValue: number = 100000;
+  expenseOptions: Options = {
+    floor: 100000,
+    ceil: 300000
+  };
 }
